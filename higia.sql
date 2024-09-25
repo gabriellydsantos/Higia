59b5ca79640e7c4f2332ebd90801a2dacc9ebec6
gabriellydsantos <gabydsantos24@gmail.com>
2024-09-25 09:35:33 -0300

    teste

--- higia.sql
+++ higia.sql
@@ -66,6 +66,7 @@ CREATE TABLE patients (
 
 -- ALTER TABLE patients MODIFY COLUMN cpf VARCHAR(14);
 
+
 -- Tabela de médicos
 CREATE TABLE doctors (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -90,6 +91,7 @@ CREATE TABLE doctors (
     rg VARCHAR(12),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 
 -- Tabela de departamentos
 CREATE TABLE departments (
@@ -125,5 +127,6 @@ CREATE TABLE staff (
     rg VARCHAR(12),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 
 drop database higia;
